439e03ea25d39b5382607f5f1a72f0f0067cd307
Artur Trapp <artur.trapp@definedcrowd.com>
2021-05-11 12:05:52 +0100

    Fixing scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-set node=/home/ec2-user/.nvm/versions/node/v16.1.0/bin/node
-set npm=/home/ec2-user/.nvm/versions/node/v16.1.0/bin/npm
-
 cd .. && npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,1 @@
-set node=/home/ec2-user/.nvm/versions/node/v16.1.0/bin/node
-set npm=/home/ec2-user/.nvm/versions/node/v16.1.0/bin/npm
-
-cd .. && npm -v start
+cd .. && npm start
