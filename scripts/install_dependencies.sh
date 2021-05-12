def81382dfdadfdfc4ba41031f5e7a4a0940c01c
Artur Trapp <artur.trapp@definedcrowd.com>
2021-05-12 10:15:33 +0100

    Updating scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 # First, lets kill all the nohup / node processes
 for PID in `ps -ef | grep nohup | awk '{print $2}'`;
-    do kill PID
+    do kill PID;
 
 for PID in `ps -ef | grep node | awk '{print $2}'`;
-    do kill PID
+    do kill PID;
 
 
 npm --prefix /var/www/html install /var/www/html
